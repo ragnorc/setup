82e4b74bd92ec3ec4552bb67d389c10abc81af30
Ragnor Comerford <ragnor.comerford@gmail.com>
2022-05-17 11:00:26 +0100

    Update z_macos_setup.sh

--- run/after/z_macos_setup.sh
+++ run/after/z_macos_setup.sh
@@ -27,68 +27,11 @@ echo "Updating Dock..."
 
 set -x
 dockutil --remove all --no-restart
-dockutil --add /Applications/Firefox.app --no-restart
-dockutil --add /Applications/Emacs.app --no-restart
-dockutil --add /Applications/Alacritty.app --no-restart
+dockutil --add /Applications/Orion.app --no-restart
+dockutil --add /Applications/Safari.app --no-restart
 dockutil --add ~/Downloads --display stack # Implicitly restarts the Dock.
 set +x
 
-echo "Updating default application handlers..."
-
-set -x
-duti -s org.gnu.Emacs com.apple.property-list all
-duti -s org.gnu.Emacs com.apple.xcode.strings-text all
-duti -s org.gnu.Emacs com.netscape.javascript-source all
-duti -s org.gnu.Emacs net.daringfireball.markdown all
-duti -s org.gnu.Emacs public.c-header all
-duti -s org.gnu.Emacs public.c-plus-plus-source all
-duti -s org.gnu.Emacs public.c-source all
-duti -s org.gnu.Emacs public.comma-separated-values-text all
-duti -s org.gnu.Emacs public.data all
-duti -s org.gnu.Emacs public.json all
-duti -s org.gnu.Emacs public.objective-c-source all
-duti -s org.gnu.Emacs public.perl-script all
-duti -s org.gnu.Emacs public.plain-text all
-duti -s org.gnu.Emacs public.precompiled-c-header all
-duti -s org.gnu.Emacs public.python-script all
-duti -s org.gnu.Emacs public.ruby-script all
-duti -s org.gnu.Emacs public.shell-script all
-duti -s org.gnu.Emacs public.swift-source all
-duti -s org.gnu.Emacs public.unix-executable all
-duti -s org.gnu.Emacs public.xml all
-duti -s org.gnu.Emacs public.yaml all
-set +x
-
-(
-    cd "$SCRIPT_DIR/../lib"
-
-    echo "Adding Terminal theme..."
-    if ! defaults read com.apple.Terminal "Window Settings" | grep -Fw Zenburn >/dev/null; then
-        open -a Terminal assets/Zenburn.terminal
-    fi
-
-    echo "Applying icons..."
-    ./set-icon assets/Alacritty.icns /Applications/Alacritty.app
-    ./set-icon assets/Emacs.icns /Applications/Emacs.app
-
-    echo "Installing custom utilities..."
-    ./KeyBindings/install
-    ./LaunchTerm/install
-)
-
-echo "Adding applications to Gatekeeper whitelist..."
-(while read -r app; do
-     spctl --remove "$app" 2>/dev/null ||:
-     spctl --add "$app"
-     echo "Whitelisted $app."
- done) <<-EOF
-	/Applications/Alacritty.app
-	/Applications/Chromium.app
-	/Applications/Pine.app
-	$HOME/Library/QuickLook/Pacifist.qlgenerator
-	$HOME/Library/QuickLook/QLColorCode.qlgenerator
-	$HOME/Library/QuickLook/QLStephen.qlgenerator
-EOF
 
 echo "Resetting QuickLook."
 set -x
